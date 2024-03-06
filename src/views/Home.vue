2aaf80fdd18fc384bd4744ed2ecfc97a52086975
glink25 <49364151+glink25@users.noreply.github.com>
2024-03-06 11:32:19 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import Counter from '@/components/Counter.vue';
+
+const { t, locale } = useI18n({
+  inheritLocale: true,
+  useScope: 'local',
+});
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    {{ $t('hello') }}
+    <div class="i-mdi-home" />
+  </div>
+  <Counter />
+</template>
